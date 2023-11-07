43961b9ca85c932b0505c0c21f6f0499b989c527
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-07 21:30:19 +1300

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import cart_icon from "@/assets/cart_icon.png";
 import logo from "@/assets/logo.png";
