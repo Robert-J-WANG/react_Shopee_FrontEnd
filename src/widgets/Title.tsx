cf4c7dc5f685fb77c4c9220832edffcd1c61eb20
Robert-J-WANG <robin.nz420@gmail.com>
2024-09-14 16:59:56 +1200

    update title

--- src/widgets/Title.tsx
+++ src/widgets/Title.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Title({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col items-center justify-center gap-5">
+    <div className="flex flex-col items-center justify-center gap-2">
       <h1 className="text-lg font-bold xl:text-3xl lg:text-2xl md:text-xl">
         {children}
       </h1>
-      <progress className="w-40 xl:w-56 lg:w-52 md:w-48 progress"></progress>
+      <progress className=" progress"></progress>
     </div>
   );
 }
